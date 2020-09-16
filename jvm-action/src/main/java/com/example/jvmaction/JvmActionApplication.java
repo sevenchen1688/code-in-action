33ce7a4279c45309db14b87bd7eff2a3f103deb2
chenzl <chenzl@ligeit.com>
2020-09-16 14:05:16 +0800

    初始化提交

---
+++ jvm-action/src/main/java/com/example/jvmaction/JvmActionApplication.java
@@ -0,0 +1,13 @@
+package com.example.jvmaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JvmActionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JvmActionApplication.class, args);
+	}
+
+}
---
+++ jvm-action/src/test/java/com/example/jvmaction/JvmActionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jvmaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JvmActionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
