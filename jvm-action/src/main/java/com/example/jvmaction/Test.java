250af54e465d2a4896808e12460382c030cecdd2
chenzl <chenzl@ligeit.com>
2020-09-19 14:42:27 +0800

    新增test类

---
+++ jvm-action/src/main/java/com/example/jvmaction/Test.java
@@ -0,0 +1,15 @@
+package com.example.jvmaction;
+
+/**
+ * @ClassName Test
+ * @Description TODO
+ * @Author chenzl
+ * @Date 2020/9/18 16:05
+ */
+public class Test {
+    private int m;
+
+    public int getM() {
+        return m + 2;
+    }
+}
