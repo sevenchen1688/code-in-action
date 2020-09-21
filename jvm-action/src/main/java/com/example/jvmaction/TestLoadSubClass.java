7822bf21cf87eda3faff383795edc5a39fc0a83b
chenzl <chenzl@ligeit.com>
2020-09-21 09:46:26 +0800

    新增spring-action

---
+++ jvm-action/src/main/java/com/example/jvmaction/TestLoadSubClass.java
@@ -0,0 +1,25 @@
+package com.example.jvmaction;
+
+import javax.sound.midi.SoundbankResource;
+
+public class TestLoadSubClass {
+    public static void main(String[] args) {
+        System.out.println(B.value);
+        A[] arr = new A[10];
+        System.out.println(A.DESC);
+    }
+}
+
+class A {
+    static {
+        System.out.println("init A ...");
+    }
+    static int value = 100;
+  	static final String DESC = "test";
+}
+
+class B extends A {
+    static {
+        System.out.println("init B ...");
+    }
+}
