b157b9b46f923875ded18a722a71c2cd06892d48
chenzl <chenzl@ligeit.com>
2020-09-24 19:17:29 +0800

    新增Druid数据库连接池demo

--- spring-in-action/src/main/java/com/example/springinaction/SpringInActionApplication.java
+++ spring-in-action/src/main/java/com/example/springinaction/SpringInActionApplication.java
@@ -1,13 +1,28 @@
 package com.example.springinaction;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
 
+import javax.sql.DataSource;
+
+@Slf4j
 @SpringBootApplication
-public class SpringInActionApplication {
+public class SpringInActionApplication implements CommandLineRunner {
+	@Autowired
+	private DataSource dataSource;
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringInActionApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		log.info(dataSource.toString());
+	}
 }
