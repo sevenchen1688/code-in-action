aeb8b0966d2bf03251811b22778d772db54e3bd3
chenzl <chenzl@ligeit.com>
2020-10-17 15:17:52 +0800

    StackOverflowError错误演示

--- jvm-action/src/main/java/com/example/jvmaction/Test.java
+++ jvm-action/src/main/java/com/example/jvmaction/Test.java
@@ -7,14 +7,15 @@ package com.example.jvmaction;
  * @Date 2020/9/18 16:05
  */
 public class Test {
-    private int m;
-
-    public int getM() {
-        System.out.println("相加：" + (m + 2));
-        return m + 2;
+    public static void main(String[] args) {
+        new Test().a();
     }
 
-    public static void main(String[] args) {
-        System.out.println("相加结果：" + (1+new Test().getM()));
+    private void a() {
+        b();
+    }
+
+    private void b() {
+        a();
     }
 }
